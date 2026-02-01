14234055b4846e66f4d8fe5462b23d9f70efc025
Liorc8 <Liorc1212@gmail.com>
2026-02-01 20:08:03 +0200

    add getAreasHandler and update areasRoutes to include it

--- server/src/routes/areasRoutes.ts
+++ server/src/routes/areasRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getAreaSummaryHandler } from "../controllers/areaSummaryController";
+import { getAreasHandler } from "../controllers/areasController";
 
 const router = Router();
 
+router.get("/", getAreasHandler);
 router.get("/:areaName/summary", getAreaSummaryHandler);
 
 export default router;

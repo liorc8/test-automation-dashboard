cae3b93b60c745b2c778ec84aff0e51366cb11c1
Liorc8 <Liorc1212@gmail.com>
2026-04-18 22:18:54 +0300

    Revert "revert: undo out-of-scope changes"
    This reverts commit 531a6d253bd1494b151365afeeaaf5b09da49bf6.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
 import RecentFailuresPage from './pages/Recentfailurespage';
